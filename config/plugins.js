e6fc5426d3d69be8452373f235fef39e817e8a00
SvipersMHD <zohaibadnan29.10@gmail.com>
2023-12-17 16:29:17 +0100

    up

---
+++ config/plugins.js
@@ -0,0 +1,15 @@
+module.exports = ({ env }) => ({
+    "netlify-deployments": {
+      enabled: true,
+      config: {
+        accessToken: "nfp_RpGfsGx3QLSynb3KJqWBgFegrFmYuh1Aeab8",
+        // sites: [
+        //   {
+        //     name: 'Site 1',
+        //     id: "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+        //     buildHook: "https://api.netlify.com/build_hooks/<hook_id>",
+        //   }
+        // ]
+      },
+    },
+  });
